c794faa6b4d5469759ecb168f53bd20a1c280741
emanuel caro <darkemlord@gmail.com>
2024-01-14 19:55:55 +0900

    refs 001 adding snake game

---
+++ src/snake_game.py
@@ -0,0 +1,35 @@
+import pygame as pg
+from random import randrange
+
+WINDOW = 800
+TILE_SIZE = 50
+RANGE = (TILE_SIZE // 2, WINDOW - TILE_SIZE // 2, TILE_SIZE)
+get_random_position = lambda: [randrange(*RANGE), randrange(*RANGE)]
+snake = pg.rect.Rect([0,0, TILE_SIZE - 2, TILE_SIZE - 2])
+snake.center = get_random_position()
+length = 1
+segments = [snake.copy()]
+snake_dir = (0,0)
+screen = pg.display.set_mode([WINDOW] * 2)
+clock = pg.time.Clock()
+
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            exit()
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_w:
+                snake_dir = (0, -TILE_SIZE)
+            if event.key == pg.K_s:
+                snake_dir = (0, TILE_SIZE)
+            if event.key == pg.K_a:
+                snake_dir = (-TILE_SIZE, 0)
+            if event.key == pg.K_d:
+                snake_dir = (TILE_SIZE, 0)
+    screen.fill("black")
+    [pg.draw.rect(screen, "green", segment) for segment in segments]
+    snake.move_ip(snake_dir)
+    segments.append(snake.copy())
+    segments = segments[-length:]
+    pg.display.flip()
+    clock.tick(60)
