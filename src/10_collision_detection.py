3339cc1ed109f4bd85a08f26572db4b3d6b9c896
emanuel caro <darkemlord@gmail.com>
2024-02-09 23:31:21 +0900

    refs 1 playing with the game flow

--- src/10_collision_detection.py
+++ src/10_collision_detection.py
@@ -12,9 +12,22 @@ pg.display.set_caption("Collision Detection")  # Set the window title
 FPS = 60
 clock = pg.time.Clock()
 
-
 # Set game Values
 VELOCITY = 5
+SCORE = 0
+
+# See All available system fonts
+fonts = pg.font.get_fonts()
+
+# Load Fonts
+
+system_font = pg.font.SysFont("calibri", 20)
+
+# Define text
+
+system_text = system_font.render(f"Score: {SCORE}", True, "white")
+system_text_rect = system_text.get_rect()
+system_text_rect.topleft = (0, 0)
 
 # Load images
 dragon_image = pg.image.load("./assets/basic_tutorial_assets/dragon_right.png")
@@ -24,8 +37,15 @@ dragon_rect.topleft = (25, 25)
 coin_image = pg.image.load("./assets/basic_tutorial_assets/coin.png")
 coin_rect = coin_image.get_rect()
 coin_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+coins = [
+    pg.Rect(
+        random.randint(0, WINDOW_WIDTH - 32),
+        random.randint(0, WINDOW_HEIGHT - 32),
+        32,
+        32,
+    )
+]
 # Main loop game
-
 running = True
 
 while running:
@@ -46,18 +66,31 @@ while running:
         dragon_rect.y += VELOCITY
 
     # Check for collision between two rect
-    if dragon_rect.colliderect(coin_rect):
-        coin_rect.left = random.randint(0, WINDOW_WIDTH - 32)
-        coin_rect.top = random.randint(0, WINDOW_HEIGHT - 32)
+    for coin_rect in coins[:]:
+        if dragon_rect.colliderect(coin_rect):
+            SCORE += 1
+            coins.append(
+                pg.Rect(
+                    random.randint(0, WINDOW_WIDTH - 32),
+                    random.randint(0, WINDOW_HEIGHT - 32),
+                    32,
+                    32,
+                )
+            )
+            coin_rect.left = random.randint(0, WINDOW_WIDTH - 32)
+            coin_rect.top = random.randint(0, WINDOW_HEIGHT - 32)
+            system_text = system_font.render(f"Score: {SCORE}", True, (255, 255, 255))
+
     # Fill display surface
     display_surface.fill((0, 0, 0))
 
-    # Draw rectangles to represent the rect's of each object
-    pg.draw.rect(display_surface, (0, 255, 0), dragon_rect, 1)
-    pg.draw.rect(display_surface, (255, 255, 0), coin_rect, 1)
+    for coin_rect in coins:
+        display_surface.blit(coin_image, coin_rect)
+        # pg.draw.rect(display_surface, (255, 255, 0), coin_rect, 1)
 
+    # pg.draw.rect(display_surface, (0, 255, 0), dragon_rect, 1)
     # Blit assets
-    display_surface.blit(coin_image, coin_rect)
+    display_surface.blit(system_text, system_text_rect)
     display_surface.blit(dragon_image, dragon_rect)
 
     # Update display
