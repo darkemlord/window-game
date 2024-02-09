1aa2f4ee2e581e91b5648518f887984cfc6e0ba8
emanuel caro <darkemlord@gmail.com>
2024-02-09 23:03:24 +0900

    refs #1 adding collision example

---
+++ src/10_collision_detection.py
@@ -0,0 +1,70 @@
+import pygame as pg, random
+
+# Initialize pygame
+pg.init()
+
+# Display surface
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 300
+display_surface = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pg.display.set_caption("Collision Detection")  # Set the window title
+
+FPS = 60
+clock = pg.time.Clock()
+
+
+# Set game Values
+VELOCITY = 5
+
+# Load images
+dragon_image = pg.image.load("./assets/basic_tutorial_assets/dragon_right.png")
+dragon_rect = dragon_image.get_rect()
+dragon_rect.topleft = (25, 25)
+
+coin_image = pg.image.load("./assets/basic_tutorial_assets/coin.png")
+coin_rect = coin_image.get_rect()
+coin_rect.center = (WINDOW_WIDTH // 2, WINDOW_HEIGHT // 2)
+# Main loop game
+
+running = True
+
+while running:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            running = False
+
+    # Get a list of all keys currently being pressed down
+    keys = pg.key.get_pressed()
+
+    if keys[pg.K_LEFT] and dragon_rect.left > 0:
+        dragon_rect.x -= VELOCITY
+    if keys[pg.K_RIGHT] and dragon_rect.right < WINDOW_WIDTH:
+        dragon_rect.x += VELOCITY
+    if keys[pg.K_UP] and dragon_rect.top > 0:
+        dragon_rect.y -= VELOCITY
+    if keys[pg.K_DOWN] and dragon_rect.bottom < WINDOW_HEIGHT:
+        dragon_rect.y += VELOCITY
+
+    # Check for collision between two rect
+    if dragon_rect.colliderect(coin_rect):
+        coin_rect.left = random.randint(0, WINDOW_WIDTH - 32)
+        coin_rect.top = random.randint(0, WINDOW_HEIGHT - 32)
+    # Fill display surface
+    display_surface.fill((0, 0, 0))
+
+    # Draw rectangles to represent the rect's of each object
+    pg.draw.rect(display_surface, (0, 255, 0), dragon_rect, 1)
+    pg.draw.rect(display_surface, (255, 255, 0), coin_rect, 1)
+
+    # Blit assets
+    display_surface.blit(coin_image, coin_rect)
+    display_surface.blit(dragon_image, dragon_rect)
+
+    # Update display
+    pg.display.update()
+
+    # Tick the  clock
+    clock.tick(FPS)
+
+# End game
+pg.quit()
